67c4f543d569ff3f77a0a8d0e39b226af3b5692e
gsmoraess <moraesreginaldo624@gmail.com>
2025-05-24 21:35:59 -0300

    Add files via upload

---
+++ Adicao.cs
@@ -0,0 +1,10 @@
+﻿namespace Adicao
+{
+    public static class Adicao
+    {
+        public static int Somar(int valor1, int valor2)
+        {
+            return valor1 + valor2;
+        }
+    }
+}
