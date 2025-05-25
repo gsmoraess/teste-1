78cbe00280809426aac924dfd61bcf883cc8a83d
gsmoraess <moraesreginaldo624@gmail.com>
2025-05-24 21:34:49 -0300

    Add files via upload

---
+++ AdicaoTests.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+
+namespace Adicao.Tests
+{
+    public class AdicaoTests
+    {
+        [Fact]
+        public void Somar_DeveRetornarSomaDeDoisNumeros()
+        {
+            
+            int valor1 = 5;
+            int valor2 = 5;
+            int resultado = Adicao.Somar(valor1, valor2);
+
+            Assert.Equal(10, resultado);
+        }
+    }
+}
